c3089c2f5d278971457200960d24f2e10c817e60
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-06 20:16:43 +0200

    logic ok, probems with classname and classlist add

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ const container = document.querySelector(".container");
 // ciclo per creare i box contenenti i numeri
 
 for (let i = 0; i < 100; i++) {
-    console.log(i+1);
-    const card = `<div class="card"> ${i+1} </div>`;
-    console.log(card);
+    const card = document.createElement("div");
+
+    // aggiungo la classe card per assicurarmi che tutti i numeri abbiano lo stesso stile
+
+    card.className = "card";
+
+    card.append(i);
+
+    container.append(card);
 }
